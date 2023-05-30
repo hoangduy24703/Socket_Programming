63e6bfa19dc50657e1191d4a47b233016338624f
Đinh Hoàng Duy <134955985+hoangduy24703@users.noreply.github.com>
2023-05-30 20:45:45 +0700

    Add files via upload

---
+++ SOCKET_PROGAMING/Client/Client.py
@@ -0,0 +1,221 @@
+﻿import socket
+import time
+                
+def checkid(idcard):
+    digits = 0
+    for i in range(0,len(idcard)):
+        if idcard[i].isdigit():
+            digits = digits + 1
+        else:
+            return False
+    if (digits == 10):
+        return True
+    else:
+        return False  
+
+def checkorder(list1):
+   s1 = list1.replace("[","")
+   s2 = s1.replace("]","")
+   s3 = s2.replace("'","")
+   s4 = s3.replace(",","")
+   newlist=[]
+   mylist = s4.split()
+   for i in range(1,5):
+        name = convertFood(str(i))
+
+        count1 = mylist.count(name)
+        if (count1 >= 1):
+            num = 0
+            for i in range(0,count1):
+                ind = mylist.index(name)
+                num += int(mylist[ind + 1])
+                mylist.pop(ind)
+
+            seq1 = (name, str(num))
+            seq2 = " ".join(seq1)
+            newlist.append(seq2) 
+
+   return newlist
+
+def recvImg(name,client,steps):
+    try:
+        file = open(name,"wb")
+        img_chunk = client.recv(1024)
+
+        while steps != 0:
+            file.write(img_chunk)
+            img_chunk = client.recv(1024)
+            steps -=1
+    except:
+        file.close()
+
+
+def convertFood (name):
+    if name == "1":
+        name = "Beef_Steak"
+    if name == "2":
+        name = "Spaghetti"
+    if name == "3":
+        name = "Pasta"
+    if name == "4":
+        name = "Pizza"
+    if name == "5":
+        name = "Burger"
+    return name
+
+
+HOST = 'localhost'
+PORT = 8000
+FORMAT = 'utf-8'
+server_address = (HOST,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(server_address)
+
+resp = client.recv(1024).decode()
+print(resp)
+addr = client.recv(1024).decode()
+
+
+introOrder = "Please enter the ordinal number and quantity of the food you want to order"
+introOrder2 = "Please enter the ordinal number and quantity of the food you want to order extra"
+connected = True
+while connected:
+    try:
+        orderList = []
+        temp = 0
+        option = int(input("Option: "))
+
+        while (option != 5):
+            if option == 1:
+                client.sendall(bytes("Menu",FORMAT))
+                resp = client.recv(1024).decode()
+                print(resp)
+
+                food = client.recv(1024).decode()
+                Food = food.split(" ")
+
+                for i in range (0,len(Food)):
+                    steps = int(client.recv(1024).decode())
+                    recvImg(Food[i],client,steps)
+            
+            elif (option == 2):
+                orderList.clear()
+                temp = 1
+                print(introOrder)
+                check = ""
+                while True:
+
+                    if check == "N" or check == "n":
+                        break
+                    name = input("Dish: ")
+                    name = convertFood(name)
+                    quantity = input("Quantity: ")
+
+                    seq = (name,quantity)
+                    order = " ".join(seq)
+                    orderList.append(order)
+
+                    print("Keep ordering [Y/N]: ")
+                    check = input()
+                
+                req = "\n".join(orderList)
+                req += "\nend"
+
+                client.sendall(bytes("Order",FORMAT))
+                rep = client.recv(1024).decode(FORMAT)
+                print(rep)
+
+                client.sendall(bytes(req,FORMAT))
+                resp = client.recv(1024).decode()
+                print(resp)
+                
+            elif (option == 3):
+
+               if (temp == 0):
+                    print("No order before. Please try again!!!")
+                    break
+               
+               print("Choose your payments: ")
+               print("1. Pay cash")
+               print("2. Credit card (Include 10 numbers)") 
+
+               option2 = input("Option payment: ")
+
+               if (option2 == "1"):
+                    client.sendall(bytes("Payment",FORMAT))
+                    client.sendall(bytes("1",FORMAT))
+                    print("Your option is paying cash.")
+               if (option2 == "2"):
+                    print("Your option is credit card.")
+                    idcard=input("Please enter your id card:  ")
+
+                    while (checkid(idcard) == False):
+                        print("Your id card is invalid")
+                        print("Please enter your id card again: ")  
+                        idcard=input()
+
+                    client.sendall(bytes("Payment",FORMAT))
+                    client.sendall(bytes("2",FORMAT))
+                    client.sendall(bytes(idcard,FORMAT))
+                    print("Your id card is valid.")
+
+               orderList = checkorder(str(orderList))
+               orderList.insert(0,addr)
+               orderList.append("Already paid")
+
+               bill = " ".join(orderList)
+               client.sendall(bytes(bill,FORMAT))
+
+               temp = 0
+               orderList.clear()
+
+               option = 5
+               break
+            elif (option == 4):
+                if (temp == 0):
+                    print("No order before. Please try again!!!")
+                    break
+                print(introOrder2)
+                check = ""
+                while True:
+
+                    if check == "N" or check == "n":
+                        break
+                    
+                    name = input("Dish: ")
+                    name = convertFood(name)
+                    quantity = input("Quantity: ")
+
+                    seq = (name,quantity)
+                    order = " ".join(seq)
+                    orderList.append(order)
+
+                    print("Keep ordering [Y/N]: ")
+                    check = input()
+            
+                req = "\n".join(orderList)
+                req += "\nend"
+                client.sendall(bytes("Extra",FORMAT))
+                rep = client.recv(1024).decode(FORMAT)
+                print(rep)
+
+                client.sendall(bytes(req,FORMAT))
+                resp = client.recv(1024).decode()
+                print("Latest",resp)
+
+            option = int(input("Option: "))
+            
+        if (option == 5):
+            client.sendall(bytes("Finish",FORMAT))
+            resp = client.recv(1024).decode()
+            print(resp)
+            time.sleep(5)
+            connected = False
+            client.close()
+            break
+    except:
+        client.close()
+
+
+
---
+++ SOCKET_PROGAMING/Server/Server.py
@@ -0,0 +1,209 @@
+﻿import socket
+import threading
+import json
+
+try:
+    file = open("menu.txt", mode ='r', encoding='utf-8')
+    menu = file.read()    
+finally:
+    file.close()
+
+Line = []
+Line = menu.split("\n")
+
+def Price(menu,food):
+    Line = []
+    Line = menu.split("\n")
+    Line.pop(0)
+    orderList = []
+
+    for i in range(0,len(Line)):
+        Line[i]=" ".join(Line[i].split())
+    i = 0
+
+    while len(Line)-1 > i:
+        orderList.append ([])
+        orderList[i] = Line[i].split(" ")
+    
+        tmp = []
+        tmp = orderList[i][0].split("-")
+        orderList[i][0] = tmp[1]
+
+        tmp = []    
+        tmp = orderList[i][1].split("$")
+        orderList[i][1] = tmp[0]
+        
+        i+=1
+        
+    i = 0
+    while food != orderList[i][0]:
+        i+=1
+    
+    return int(orderList[i][1])
+def Bill(req):
+    Line = []
+    Line = req.split("\n")
+    orderList = []
+
+    i = 0
+    while True:
+        orderList.append([])
+        orderList[i] = Line[i].split(" ")
+        if orderList[i][0] == "end":
+            break
+        i+=1
+        
+    bill = 0
+    j = 0
+    
+    for j in range(0,i):
+        bill += (Price(menu,orderList[j][0])*int(orderList[j][1]))
+    
+    return "Bill: " + str(bill) + "$."
+def sendImg(name,client):
+    try:
+        file = open(name,"rb")
+        img_data = file.read(1024)
+
+        while img_data:
+            client.sendall(bytes(img_data))
+            img_data = file.read(1024)
+    except:
+        file.close()
+def sendImgSteps(name):
+    try:
+        file = open(name,"rb")
+        img_data = file.read(1024)
+        steps = -1 
+        while img_data:
+            steps+=1
+            img_data = file.read(1024)
+    except:
+        file.close()
+
+    return steps
+def imgName(menu):
+    Line = []
+    Line = menu.split("\n")
+    Line.pop(0)
+    List = []
+    Food = []
+    for i in range(0,len(Line)):
+        Line[i]=" ".join(Line[i].split())
+    i = 0
+
+    while len(Line)-1 > i:
+        List.append ([])
+        List[i] = Line[i].split(" ")
+    
+        tmp = []
+        tmp = List[i][0].split("-")
+        tmp[1] += ".jpg"
+        Food.append(tmp[1])
+        i+=1
+    
+    strFood = " ".join(Food)
+    return strFood  
+def saveBill(bill):
+    with open('billList.json','r+') as f:
+        file_data = json.load(f)
+        file_data["Bill"].append(bill)
+        f.seek(0)
+        json.dump(file_data, f, indent = 4)
+ 
+#-------------------------------------------------------------
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 8000
+FORMAT = 'utf-8'
+SIZE = 1024
+
+#----------------------------------------------------
+def handle_client(client, addr):
+    try:
+        print(f"[NEW CONNECTION] {addr} connected.")
+        #---------------TEXT---------------
+        intro = "Welcome to Mr.Bean \n\n View menu_Enter: 1 \n Order food_Enter: 2 \n Payment method : 3 \n Extra order (Just before 2 hours) : 4 \n Finish : 5 \n"
+        #----------------------------------
+
+        connected = True
+        while connected:
+            client.sendall(bytes(intro,FORMAT))
+            client.sendall(bytes(str(addr),FORMAT))
+            req = client.recv(1024).decode(FORMAT)
+            while (req != "Finish"):
+                if req == "Menu":
+                    client.sendall(bytes(menu,FORMAT))
+                    client.sendall(bytes(imgName(menu),FORMAT))
+                    Food = imgName(menu).split(" ")  
+                    for i in range (0,len(Food)):
+                        client.sendall(bytes(str(sendImgSteps(Food[i])),FORMAT))
+                        sendImg(Food[i],client)
+                elif req == "Order":
+                    client.sendall(bytes("Order accepted",FORMAT))
+                    req = client.recv(1024).decode()
+                    client.sendall(bytes(Bill(req),FORMAT)) 
+                elif req == "Payment": 
+                    option = client.recv(1024).decode(FORMAT)
+                    if (option == "2"):
+                        print(f"[CONNECTION] {addr} [PAY]")
+                        idcard = client.recv(1024).decode(FORMAT)
+                        bill = client.recv(1024).decode(FORMAT)
+                        print("[RECEIVED]")
+                        print(bill)
+                        saveBill(bill)
+                    else:
+                        print(f"[CONNECTION] {addr} [PAY]")
+                        bill = client.recv(1024).decode(FORMAT)
+                        print("[RECEIVED]")
+                        print(bill)
+                        saveBill(bill)
+                elif req == "Extra":
+                    client.sendall(bytes("Extra order accepted",FORMAT))
+                    req = client.recv(1024).decode()
+                    client.sendall(bytes(Bill(req),FORMAT))
+        
+                req = client.recv(1024).decode(FORMAT)
+      
+            if req == "Finish":
+                print(addr,"disconnected!")
+                client.sendall(bytes("Thanks for choosing us!",FORMAT))
+                connected= False
+    except:
+        print(addr,"disconnected!")
+        client.close()
+
+#-----------------------------------------------------
+def main():
+
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HOST = 'localhost'
+    server.bind((HOST,PORT))
+    
+    print("[STATUS]Server created")
+
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+    print("[STATUS]Waiting for connection \n")
+
+    data={
+        "Bill":[
+        ]
+    }
+    with open('billList.json','w') as f:
+        json.dump(data,f,indent=4)
+
+    while True:
+        client, addr = server.accept()
+        print("Connected by ", addr)
+            
+        thread = threading.Thread(target=handle_client, args=(client, addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS]", threading.activeCount() - 1)
+
+    server.close()
+
+if __name__ == "__main__":
+    main()
+    
